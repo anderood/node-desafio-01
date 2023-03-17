9af92c3b10f0f91b0d1d8d7e552261301d59a992
Anderson Santos <anderood@hotmail.com>
2023-03-16 22:06:59 -0300

    complete get/todos

--- src/index.js
+++ src/index.js
@@ -50,7 +50,11 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request;
+
+  const dataUser = users.find(user => user.username === username);
+
+  return response.status(200).json(dataUser.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
